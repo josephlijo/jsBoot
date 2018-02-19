9149b3df23e6926920f573fe7222f8cb516e8556
LJNotes <ljnotes@outlook.com>
2018-02-19 14:08:33 +0000

    Include code for mocking HTTP call

--- Learning/jsTesting/TestingWithMocha/04.GitServiceApp/test/services/git.user.service.http.spec.js
+++ Learning/jsTesting/TestingWithMocha/04.GitServiceApp/test/services/git.user.service.http.spec.js
@@ -2,14 +2,35 @@ var chai = require('chai');
 var sinon = require('sinon');
 
 var https = require('https');
+var PassThrough = require('stream').PassThrough;
 
 chai.should();
 var gitUserService = require('../../services/git.user.service');
 
 describe('Git Service', function () {
     describe('Get Git User', function () {
+        beforeEach('Setup', function () {
+            // Stub just the `request` function of `https`
+            this.request = sinon.stub(https, 'request');
+        });
         // Test case: Get user information from Git
         it('Should return user information', function () {
+            // Arrange...
+            // Dummy result
+            var responseJSON = {
+                id: 1,
+                name: 'Foo Bar',
+                userName: 'ljnotes'
+            };
+            var fakeResponse = new PassThrough();
+            fakeResponse.write(JSON.stringify(responseJSON));
+            fakeResponse.end();
+
+            // Act...
+            // `request` is stubbed and we are making telling it to return 
+            // JSON string 
+            this.request.callsArgWith(1, fakeResponse).returns(new PassThrough());
+
             return gitUserService.getUserInfo('ljnotes').then(function (userData) {
                 userData.should.have.property('userName');
                 userData.userName.should.equal('ljnotes');
@@ -17,5 +38,9 @@ describe('Git Service', function () {
                 console.log(error);
             });
         });
+        afterEach('tear off and restore', function () {
+            // Restore the `hijacked` https request object.
+            this.request.restore();
+        })
     });
 });
