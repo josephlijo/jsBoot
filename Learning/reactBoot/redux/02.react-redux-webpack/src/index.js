bd1f10fd1c3941620ca1241a2b725c51fb576a16
LJNotes <ljnotes@outlook.com>
2018-03-05 15:34:06 +0000

    Connect redux store with component

--- Learning/reactBoot/redux/02.react-redux-webpack/src/index.js
+++ Learning/reactBoot/redux/02.react-redux-webpack/src/index.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
+import store from './stores/configureStore'
 
 class GoalScorer extends React.Component {
     constructor(props) {
@@ -12,22 +13,16 @@ class GoalScorer extends React.Component {
     }
 
     increment() {
-        this.setState((prevState, props) => {
-            return { counter: prevState.counter + 1 }
-        })
     }
 
     decrement() {
-        this.setState((prevState, props) => {
-            return { counter: prevState.counter - 1 };
-        })
     }
 
     render() {
         return (
             <div>
                 <button onClick={this.decrement}>-</button>
-                <input type='text' value={this.state.counter} />
+                <input type='text' value={store.getState().counter} />
                 <button onClick={this.increment}>+</button>
             </div>
         );
---
+++ Learning/reactBoot/redux/02.react-redux-webpack/src/stores/configureStore.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux';
+
+var defaultState = {
+    counter: 3
+};
+
+var actions = {
+    INCREMENT: 'INCREMENT',
+    DECREMENT: 'DECREMENT'
+};
+
+var scoreChanger = function(state = defaultState, action) {
+    if(action.type == actions.INCREMENT) {
+        return {
+            counter: state.counter + 1
+        }
+    } else if (action.type == actions.DECREMENT) {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    return state;
+}
+
+var store = createStore(scoreChanger);
+export default store;
